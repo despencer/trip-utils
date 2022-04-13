ee6f2754ad938151c91e418f85cf2c69b300e812
despencer <despencer@mail.ru>
2022-04-13 16:40:14 +0300

    Little refactoring: move all obf-loading data into obfdata.py

--- obfdata.py
+++ obfdata.py
@@ -1,14 +1,11 @@
 import math
+from datetime import datetime, timedelta
+from schemareader import Schema, SchemaReader
+from pbreader import ProtobufReader
 import geo
 
-# longitude from integer to degrees
-def lonitod(x):
-    return (x * 360.0 / (1024.0 * (1<<21))) - 180.0
-
-def latitod(y):
-    sign = -1 if y < 0 else 1
-    arg = math.pi * ( 1.0 - ( y / (512.0 * (1<<21))  ) )
-    return math.atan(sign * math.sinh(arg)) * 180.0 / math.pi
+def unixtime(x):
+    return datetime(1970,1,1)+timedelta(milliseconds=x)
 
 class Map:
     def __init__(self):
@@ -86,3 +83,36 @@ class MapNode:
 class MapBlock:
     def __init__(self):
         pass
+
+
+obschema = { 'start':'header', 'structures':[
+        { 'name':'header', 'factory': Map, 'fields':
+    { 1 : { 'name':'version' }, 18:{'name':'creation', 'factory':unixtime},
+    6:{ 'name':'sections', 'factory':Section, 'children':{     # map_section
+        2:{'name':'name', 'factory':ProtobufReader.readutf8},
+        5:{'name':'maplevels', 'structure':'maplevel' } } } } },
+    { 'name':'maplevel', 'factory':MapLevel, 'fields':{
+            1:{'name':'maxzoom'}, 2:{'name':'minzoom'}, 3:{'name':'ibounds.left.value'}, 4:{'name':'ibounds.right.value'}, 5:{'name':'ibounds.top.value'}, 6:{'name':'ibounds.bottom.value'},
+            7:{'name':'_node', 'lazy':'_nodereader', 'structure':'treenode' } } },
+    { 'name':'treenode', 'factory':MapNode, 'fields': {
+        1:{'name':'ibounds.left.value', 'factory':ProtobufReader.readzigzag}, 2:{'name':'ibounds.right.value', 'factory':ProtobufReader.readzigzag},
+        3:{'name':'ibounds.top.value', 'factory':ProtobufReader.readzigzag}, 4:{'name':'ibounds.bottom.value', 'factory':ProtobufReader.readzigzag},
+        5:{'name':'_block', 'lazy':'_blockreader', 'structure':'mapblock'}, 7:{'name':'_children', 'lazy':'_childrenreader','structure':'treenode'} }},
+    { 'name':'mapblock', 'factory':MapBlock, 'fields':{} } ] }
+
+
+# longitude from integer to degrees
+def lonitod(x):
+    return (x * 360.0 / (1024.0 * (1<<21))) - 180.0
+
+def latitod(y):
+    sign = -1 if y < 0 else 1
+    arg = math.pi * ( 1.0 - ( y / (512.0 * (1<<21))  ) )
+    return math.atan(sign * math.sinh(arg)) * 180.0 / math.pi
+
+def readobf(obfile):
+    schema = Schema(obschema)
+#    schema.addformat({ 'structures':[ { 'name':'header', 'fields': { 6:{'print':4, 'children': { 5: { 'print':0 } } } } } ] } )
+    reader = SchemaReader(obfile, schema)
+    return reader.read()
+
--- obfinfo.py
+++ obfinfo.py
@@ -3,30 +3,8 @@
 import sys
 import os
 import logging
-from datetime import datetime, timedelta
-from schemareader import Schema, SchemaReader
-from pbreader import ProtobufReader
 sys.path.insert(1, os.path.abspath('../geo'))
 import obfdata
-import geo
-
-def unixtime(x):
-    return datetime(1970,1,1)+timedelta(milliseconds=x)
-
-obschema = { 'start':'header', 'structures':[
-        { 'name':'header', 'factory': obfdata.Map, 'fields':
-    { 1 : { 'name':'version' }, 18:{'name':'creation', 'factory':unixtime},
-    6:{ 'name':'sections', 'factory':obfdata.Section, 'children':{     # map_section
-        2:{'name':'name', 'factory':ProtobufReader.readutf8},
-        5:{'name':'maplevels', 'structure':'maplevel' } } } } },
-    { 'name':'maplevel', 'factory':obfdata.MapLevel, 'fields':{
-            1:{'name':'maxzoom'}, 2:{'name':'minzoom'}, 3:{'name':'ibounds.left.value'}, 4:{'name':'ibounds.right.value'}, 5:{'name':'ibounds.top.value'}, 6:{'name':'ibounds.bottom.value'},
-            7:{'name':'_node', 'lazy':'_nodereader', 'structure':'treenode' } } },
-    { 'name':'treenode', 'factory':obfdata.MapNode, 'fields': {
-        1:{'name':'ibounds.left.value', 'factory':ProtobufReader.readzigzag}, 2:{'name':'ibounds.right.value', 'factory':ProtobufReader.readzigzag},
-        3:{'name':'ibounds.top.value', 'factory':ProtobufReader.readzigzag}, 4:{'name':'ibounds.bottom.value', 'factory':ProtobufReader.readzigzag},
-        5:{'name':'_block', 'lazy':'_blockreader', 'structure':'mapblock'}, 7:{'name':'_children', 'lazy':'_childrenreader','structure':'treenode'} }},
-    { 'name':'mapblock', 'factory':obfdata.MapBlock, 'fields':{} } ] }
 
 def printnodes(indent, node):
     if len(node.children) == 0:
@@ -35,12 +13,9 @@ def printnodes(indent, node):
     for c in node.children:
         printnodes(indent+'    ', c)
 
-def main2(fname):
-    schema = Schema(obschema)
-#    schema.addformat({ 'structures':[ { 'name':'header', 'fields': { 6:{'print':4, 'children': { 5: { 'print':0 } } } } } ] } )
+def main(fname):
     with open(fname, 'rb') as obfile:
-        reader = SchemaReader(obfile, schema)
-        obfmap = reader.read()
+        obfmap = obfdata.readobf(obfile)
         print('Map version {0}'.format(obfmap.version))
         for s in obfmap.sections:
             print('Section {0}'.format(s.name))
@@ -50,5 +25,5 @@ def main2(fname):
 
 if __name__ == '__main__':
     logging.basicConfig(filename='obinfo.log', filemode='w', level=logging.INFO)
-    main2('/mnt/mobihome/maps/Russia_central-federal-district_asia_2.obf')
+    main('/mnt/mobihome/maps/Russia_central-federal-district_asia_2.obf')
 
