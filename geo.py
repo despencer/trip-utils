40e4c2e26bf87304c6fc9de98014e7863a7738a3
despencer <despencer@mail.ru>
2022-05-02 00:12:51 +0300

    Added bounds as point margins and bound's intersection.

--- geo.py
+++ geo.py
@@ -26,6 +26,15 @@ class Point:
         ret.lon.value = lon
         return ret
 
+    def boundslatlon(self, lat, lon):
+        """ returns bounds around the point with the lat and lon margins"""
+        bounds = Rectangle()
+        bounds.left.value = self.lon.value - lon
+        bounds.top.value = self.lat.value + lat
+        bounds.right.value = self.lon.value + lon
+        bounds.bottom.value = self.lat.value - lat
+        return bounds
+
 class Rectangle:
     def __init__(self):
         self.left = Coordinate()
@@ -41,3 +50,9 @@ class Rectangle:
     def isinside(self, point):
         return self.left.value <= point.lon.value and point.lon.value <= self.right.value and self.bottom.value <= point.lat.value and point.lat.value <= self.top.value
 
+    def isintersects(self, arect):
+        return ((self.left.value <= arect.left.value and arect.left.value <= self.right.value) or
+                (self.left.value <= arect.right.value and arect.right.value <= self.right.value) or
+                (arect.left.value <= self.left.value and self.right.value <= arect.right.value)) and ((self.bottom.value <= arect.top.value and arect.top.value <= self.top.value) or
+                (self.bottom.value <= arect.bottom.value and arect.bottom.value <= self.top.value) or
+                (arect.bottom.value <= self.bottom.value and self.top.value <= arect.top.value))
