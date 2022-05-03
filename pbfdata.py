3510464dcb8040bee2489c066dee4ea194d19ac4
despencer <despencer@mail.ru>
2022-05-04 00:07:57 +0300

    Refactor osmreader into a class.

--- pbfdata.py
+++ pbfdata.py
@@ -14,17 +14,25 @@ class BlobHeader:
 headerschema = { 'start':'header', 'structures':[
         { 'name':'header', 'factory': BlobHeader, 'fields': {1:{'name':'type', 'factory':ProtobufReader.readutf8}, 3:{'name':'datasize'} } } ] }
 
+class OsmPbfReader:
+    def __init__(self, pbfile):
+        self.pbfile = pbfile
+        self.osmfile = OsmFile()
+
+    def readblobs(self):
+        filereader = Reader(self.pbfile)
+        pos = 0
+        limit = filereader.size()
+        while True:
+            if pos >= limit:
+                return
+            headersize = filereader.readintat(pos, 4, 'big')
+            headerreader = SchemaReader(self.pbfile, Schema(headerschema))
+            header = headerreader.readsection(pos+4, headersize)
+            self.osmfile.blobs.append(header)
+            pos += 4 + headersize + header.datasize
+
 def readpbf(pbfile):
-    osmfile = OsmFile()
-    filereader = Reader(pbfile)
-    pos = 0
-    limit = filereader.size()
-    while True:
-        if pos >= limit:
-            return osmfile
-        headersize = filereader.readintat(pos, 4, 'big')
-        headerreader = SchemaReader(pbfile, Schema(headerschema))
-        header = headerreader.readsection(pos+4, headersize)
-        osmfile.blobs.append(header)
-        pos += 4 + headersize + header.datasize
-    return osmfile
+    reader = OsmPbfReader(pbfile)
+    reader.readblobs()
+    return reader.osmfile
