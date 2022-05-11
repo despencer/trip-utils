d7521b1d0042e1ec0a956bcf12f74ab7bdf74273
despencer <despencer@mail.ru>
2022-05-11 19:48:59 +0300

    Turn locator function to the class.

--- pbfselect.py
+++ pbfselect.py
@@ -16,28 +16,43 @@ class Node:
     def __repr__(self):
         return 'Node #{0} at {1}'.format(self.id, self.point)
 
-def locatenodes(osmfile, bounds):
-    ibounds = geo.Rectangle.fromltrb(round(bounds.left.value*10000000), round(bounds.top.value*10000000), round(bounds.right.value*10000000), round(bounds.bottom.value*10000000))
-    nodes = []
-    print('Bounds', bounds)
-    indy = Indicator()
-    for i, blob in enumerate(osmfile.blobs):
-        if indy.ready():
-            print(i,'/',len(osmfile.blobs), 'nodes found', len(nodes))
-        block = blob.blob.readcontents()
-        for p in block.primitives:
-            if p.densenodes != None:
-                for j in range(0, len(p.densenodes.ids)):
-                    if ibounds.left.value <= p.densenodes.lons[j] and p.densenodes.lons[j] <= ibounds.right.value and ibounds.bottom.value <= p.densenodes.lats[j] and p.densenodes.lats[j] <= ibounds.top.value:
-                        node = Node(p.densenodes.ids[j], p.densenodes.lats[j]/10000000, p.densenodes.lons[j]/10000000)
-                        nodes.append(node)
-    for n in nodes:
-        print(n)
+class Selector:
+    def __init__(self, osmfile, bounds):
+        self.osmfile = osmfile
+        self.bounds = bounds
+        self.ibounds = geo.Rectangle.fromltrb(round(bounds.left.value*10000000), round(bounds.top.value*10000000), round(bounds.right.value*10000000), round(bounds.bottom.value*10000000))
+        self.nodes = None
+        self.wayblocks = None
+
+    def locatenodes(self):
+        print('Selecting nodes for bounds', self.bounds)
+        self.nodes = {}
+        self.wayblocks = []
+        indy = Indicator()
+        for i in range(0, len(self.osmfile.blobs)):
+            blob = self.osmfile.blobs[i]
+            if indy.ready():
+                print(i,'/',len(self.osmfile.blobs), 'nodes found', len(self.nodes))
+            block = blob.blob.readcontents()
+            for p in block.primitives:
+                if len(p.ways) > 0 and (i not in self.wayblocks):
+                    self.wayblocks.append(i)
+                if p.densenodes != None:
+                    for j in range(0, len(p.densenodes.ids)):
+                        if self.ibounds.left.value <= p.densenodes.lons[j] and p.densenodes.lons[j] <= self.ibounds.right.value and self.ibounds.bottom.value <= p.densenodes.lats[j] and p.densenodes.lats[j] <= self.ibounds.top.value:
+                            node = Node(p.densenodes.ids[j], p.densenodes.lats[j]/10000000, p.densenodes.lons[j]/10000000)
+                            self.nodes[node.id] = node
+
+    def locate(self):
+        self.locatenodes()
+        print(self.wayblocks)
+        print('Total nodes', len(self.nodes), 'wayblocks', len(self.wayblocks))
 
 def main(fname):
     with open(fname, 'rb') as pbfile:
         osmfile = pbfdata.readpbf(pbfile)
-        locatenodes(osmfile, geo.Rectangle.fromltrb(37 + (31.704/60), 55 + (38.544/60), 37 + (31.904/60), 55 + (38.344/60) ))
+        selector = Selector(osmfile, geo.Rectangle.fromltrb(37 + (31.704/60), 55 + (38.544/60), 37 + (31.904/60), 55 + (38.344/60) ))
+        selector.locate()
 
 if __name__ == '__main__':
     logging.basicConfig(filename='pbroute.log', filemode='w', level=logging.INFO)
